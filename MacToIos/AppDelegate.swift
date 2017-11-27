e9fb370f71e297310cf8518fa136bf51f59e552e
WalkWithWind <956035125@qq.com>
2017-11-27 09:42:37 +0800

    Initial Commit

---
+++ MacToIos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacToIos
+//
+//  Created by sycf_ios on 2017/11/27.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacToIos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacToIos
+//
+//  Created by sycf_ios on 2017/11/27.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
